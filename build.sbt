a149bbe1331cd9a91baa2f4132942168782fc86a
Roger Deetz <rdeetz@gmail.com>
2019-01-26 09:33:29 -0500

    Added requests and responses.

--- build.sbt
+++ build.sbt
@@ -2,5 +2,12 @@ ThisBuild / organization := "mooville"
 
 lazy val traveler = (project in file(".")).settings(
     name := "Traveler",
-    libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % Test
+    libraryDependencies ++= List(
+        "com.typesafe.akka" %% "akka-stream" % "2.5.19",
+        "com.softwaremill.sttp" %% "core" % "1.5.8",
+        "com.softwaremill.sttp" %% "akka-http-backend" % "1.5.8",
+        "com.softwaremill.sttp" %% "json4s" % "1.5.8",
+        "org.json4s" %% "json4s-native" % "3.6.0",
+        "org.scalatest" %% "scalatest" % "3.0.5" % Test
+    )
 )
--- src/main/scala/mooville/traveler/Traveler.scala
+++ src/main/scala/mooville/traveler/Traveler.scala
@@ -1,5 +1,34 @@
 package mooville.traveler
 
+import com.softwaremill.sttp._
+import com.softwaremill.sttp.akkahttp._
+import com.softwaremill.sttp.json4s._
+
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+case class HttpBinResponse(origin: String, headers: Map[String, String])
+
 object Traveler extends App {
     println("Are you ready to travel?")
+
+    // val city = Some("1")
+    // val request = sttp.get(uri"http://localhost:9200/traveler/cities/$city")
+    // implicit val backend = HttpURLConnectionBackend()
+    // val response = request.send()
+    // println(response.body)
+    // println(response.headers)
+
+    // implicit val serialization = org.json4s.native.Serialization
+    // val request = sttp.get(uri"https://httpbin.org/get").response(asJson[HttpBinResponse])
+    // implicit val backend = AkkaHttpBackend()
+    // val response: Future[Response[HttpBinResponse]] = request.send()
+
+    // for (r <- response) {
+    //     println(r.code)
+    //     println(r.body)
+    //     backend.close()
+    // }
+
+    
 }
