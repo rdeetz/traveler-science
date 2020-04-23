6f0d0b9db5f677b3247b23b9230fb2c971d0ae5f
Roger Deetz <rdeetz@gmail.com>
2020-04-23 16:23:42 -0400

    Added initial connection to PostgreSQL.

--- apps/hawk/traveler.c
+++ apps/hawk/traveler.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <libpq-fe.h>
 
 int main(int argc, char* argv[])
 {
     printf("Are you ready to travel?\n");
+
+    const char* conninfo;
+    PGconn* conn;
+
+    conninfo = "dbname = traveler";
+    conn = PQconnectdb(conninfo);
+
+    if (PQstatus(conn) != CONNECTION_OK)
+    {
+        printf("You are not ready to travel.\n");
+    }
+    else
+    {
+        printf("You ARE ready to travel!\n");
+    }
+
+    PQfinish(conn);
+
     return 0;
 }
