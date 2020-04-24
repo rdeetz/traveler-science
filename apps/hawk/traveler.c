277293d8e45747a0274c5eb6ac1b4333c134a290
Roger Deetz <rdeetz@gmail.com>
2020-04-23 21:36:50 -0400

    Actually read some data.

--- apps/hawk/traveler.c
+++ apps/hawk/traveler.c
@@ -14,10 +14,32 @@ int main(int argc, char* argv[])
     if (PQstatus(conn) != CONNECTION_OK)
     {
         printf("You are not ready to travel.\n");
+        printf("Connection to database failed %s", PQerrorMessage(conn));
     }
     else
     {
         printf("You ARE ready to travel!\n");
+
+        PGresult* res = PQexec(conn, "SELECT * FROM countries");
+
+        int fields = PQnfields(res);
+
+        for (int i = 0; i < fields; i++)
+        {
+            printf("%-2s\n", PQfname(res, i));
+        }
+
+        for (int i = 0; i < PQntuples(res); i++)
+        {
+            for (int j = 0; j < fields; j++)
+            {
+                printf("%s  ", PQgetvalue(res, i, j));
+            }
+
+            printf("\n");
+        }
+
+        PQclear(res);
     }
 
     PQfinish(conn);
