c4fbafe9449c986509ad4eb83df222e78824a7f9
Roger Deetz <rdeetz@gmail.com>
2020-05-03 21:09:08 -0400

    Accepted some inputs and queried them.

--- apps/hawk/traveler.c
+++ apps/hawk/traveler.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 #include <libpq-fe.h>
 
 int main(int argc, char* argv[])
 {
+    // Accept an input, query the database, return results.
+    // Accept an input, query the search index via API, return results.
+    // Accept an input, load a CSV files, query the files, return results.
+
     printf("Are you ready to travel?\n");
 
-    const char* conninfo;
-    PGconn* conn;
+    printf("What country do you want to go to? ");
+    
+    char input[128];
+    fgets(input, 128, stdin);
+    char country[128];
+    strncpy(country, input, strlen(input) - 1);
 
-    conninfo = "dbname = traveler";
-    conn = PQconnectdb(conninfo);
+    printf("Looks like you are going to %s\n", country);
+
+    char* template = "SELECT id, country, code FROM countries WHERE country LIKE '%%%s%%'";
+    char query[128];
+    sprintf(query, template, country);
+
+    const char* conninfo = "dbname = traveler";
+
+    PGconn* conn = PQconnectdb(conninfo);
 
     if (PQstatus(conn) != CONNECTION_OK)
     {
-        printf("You are not ready to travel.\n");
         printf("Connection to database failed %s", PQerrorMessage(conn));
     }
     else
     {
-        printf("You ARE ready to travel!\n");
-
-        PGresult* res = PQexec(conn, "SELECT * FROM countries");
+        PGresult* res = PQexec(conn, query);
 
         int fields = PQnfields(res);
-
-        for (int i = 0; i < fields; i++)
-        {
-            printf("%-2s\n", PQfname(res, i));
-        }
+        // To get the name of a column in the result PQfname(res, i))
 
         for (int i = 0; i < PQntuples(res); i++)
         {
