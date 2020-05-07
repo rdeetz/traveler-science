509f394626386c059a546e36bcf79ccecc13af7a
Roger Deetz <rdeetz@gmail.com>
2020-05-06 21:57:44 -0400

    Refactorama.

--- apps/hawk/traveler.c
+++ apps/hawk/traveler.c
@@ -2,56 +2,87 @@
 #include <string.h>
 #include <libpq-fe.h>
 
-int main(int argc, char* argv[])
+int prompt_for_destination(char* destination)
 {
-    // Accept an input, query the database, return results.
-    // Accept an input, query the search index via API, return results.
-    // Accept an input, load a CSV files, query the files, return results.
-
-    printf("Are you ready to travel?\n");
-
-    printf("What country do you want to go to? ");
-    
+    printf("What country do you want to go to? ");    
     char input[128];
     fgets(input, 128, stdin);
+
     char country[128];
     strncpy(country, input, strlen(input) - 1);
-
     printf("Looks like you are going to %s\n", country);
 
+    strncpy(destination, country, strlen(country));
+
+    return strlen(country);
+}
+
+int query_database(char* country)
+{
     char* template = "SELECT id, country, code FROM countries WHERE country LIKE '%%%s%%'";
     char query[128];
     sprintf(query, template, country);
 
-    const char* conninfo = "dbname = traveler";
+    const char* connection_string = "dbname = traveler";
+    PGconn* cxn = PQconnectdb(connection_string);
 
-    PGconn* conn = PQconnectdb(conninfo);
-
-    if (PQstatus(conn) != CONNECTION_OK)
+    if (PQstatus(cxn) != CONNECTION_OK)
     {
-        printf("Connection to database failed %s", PQerrorMessage(conn));
+        printf("Connection to database failed %s", PQerrorMessage(cxn));
     }
     else
     {
-        PGresult* res = PQexec(conn, query);
+        PGresult* result = PQexec(cxn, query);
+        int fields = PQnfields(result);
+        // To get the name of a column in the result PQfname(result, i))
 
-        int fields = PQnfields(res);
-        // To get the name of a column in the result PQfname(res, i))
-
-        for (int i = 0; i < PQntuples(res); i++)
+        for (int i = 0; i < PQntuples(result); i++)
         {
             for (int j = 0; j < fields; j++)
             {
-                printf("%s  ", PQgetvalue(res, i, j));
+                printf("%s  ", PQgetvalue(result, i, j));
             }
 
             printf("\n");
         }
 
-        PQclear(res);
+        PQclear(result);
     }
 
-    PQfinish(conn);
+    PQfinish(cxn);
+
+    return 0;
+}
+
+int load_csv_file()
+{
+    FILE* file = fopen("../../data/country-codes.csv", "r");
+
+    int lineCount = 0;
+    char line[256];
+
+    while (fgets(line, 256, file) != NULL)
+    {
+        //result = fscanf(file, "%s\n", line);
+        printf("%s", line);
+        lineCount++;
+    }
+
+    printf("Number of lines: %d\n", lineCount); 
+
+    // fpos_t position;
+    // fgetpos(file, &position);
+
+    fclose(file);
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    printf("Are you ready to travel?\n");
+
+    load_csv_file();
 
     return 0;
 }
