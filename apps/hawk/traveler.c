3def0ae5012c95412f1698dd9b14f1ae3578a1e8
Roger Deetz <rdeetz@gmail.com>
2020-05-10 15:08:43 -0400

    Made some updates.

--- apps/hawk/traveler.c
+++ apps/hawk/traveler.c
@@ -9,7 +9,7 @@ int prompt_for_destination(char* destination)
     fgets(input, 128, stdin);
 
     char country[128];
-    strncpy(country, input, strlen(input) - 1);
+    strncpy(country, input, strlen(input) - 1); // Trying to remove the newline included by fgets.
     printf("Looks like you are going to %s\n", country);
 
     strncpy(destination, country, strlen(country));
@@ -28,13 +28,13 @@ int query_database(char* country)
 
     if (PQstatus(cxn) != CONNECTION_OK)
     {
-        printf("Connection to database failed %s", PQerrorMessage(cxn));
+        printf("Connection to database failed: %s", PQerrorMessage(cxn));
     }
     else
     {
         PGresult* result = PQexec(cxn, query);
         int fields = PQnfields(result);
-        // To get the name of a column in the result PQfname(result, i))
+        // To get the name of a column in the result PQfname(result, index))
 
         for (int i = 0; i < PQntuples(result); i++)
         {
@@ -54,38 +54,99 @@ int query_database(char* country)
     return 0;
 }
 
+int print_out_string(char* line)
+{
+    for (int i = 0; i < strlen(line); i++)
+    {
+        if (line[i] == '\n')
+        {
+            printf("[%d] line-feed", i);
+        }
+        else if (line[i] == '\r')
+        {
+            printf("[%d] carriage-return", i);
+        }
+        else
+        {
+            printf("[%d] %c", i, line[i]);
+        }
+        
+        printf("\n");
+    }
+
+    printf("Total length of this line: %lu\n", strlen(line));
+
+    return 0;
+}
+
 int load_csv_file()
 {
     FILE* file = fopen("../../data/country-codes.csv", "r");
 
-    int lineCount = 0;
+    int line_count = 0;
     char line[256];
 
     while (fgets(line, 256, file) != NULL)
     {
-        //result = fscanf(file, "%s\n", line);
-        //printf("%s", line);
-        lineCount++;
+        line_count++;
     }
 
-    printf("Number of lines: %d\n", lineCount);
+    printf("Number of lines in the file: %d\n", line_count);
     rewind(file);
 
-    char lines[lineCount][256];
+    int country_count = line_count;
 
-    for (int i = 0; i < lineCount; i++)
+    char tmp[256];
+    char lines[country_count][256];
+
+    for (int i = 0; i < country_count; i++)
     {
-        // char tmp[256];
-        // fgets(tmp, 256, file);
-        // strncpy(lines[i], tmp, strlen(tmp) - 1);
         fgets(lines[i], 256, file);
-        printf("%s", lines[i]);
     }
 
-    // fpos_t position;
-    // fgetpos(file, &position);
+    size_t array_length = (sizeof lines) / (sizeof lines[0]);
+    printf("Number elements in the array: %zu\n", array_length);
 
     fclose(file);
+
+    char countries[country_count][256];
+
+    for (int j = 0; j < country_count; j++)
+    {
+        if (j != 247)
+        {
+            strncpy(countries[j], lines[j], strlen(lines[j]) - 2);
+        }
+    }
+
+    for (int k = 0; k < country_count; k++)
+    {
+        int diff = strlen(lines[k]) - strlen(countries[k]);
+
+        if (diff != 2)
+        {
+            printf("This one is screwed up: %d\n", k);
+            printf("%s", lines[k]);
+            printf("Length of line: %lu / Length of country: %lu\n\n", strlen(lines[k]), strlen(countries[k]));
+        }
+    }
+
+    /*
+    // Now we can inspect elements in the array.
+    int index = 242;
+    // printf("Which country would you like to visit? ");
+    // fscanf(stdin, "%d", &index);
+    printf("Original line %s", lines[index]);
+    print_out_string(lines[index]);
+
+    // char tmp[256];
+    // strncpy(tmp, lines[index], strlen(lines[index]) - 2);
+    // printf("Here you go after trimming! %s\n", tmp); // Need to add newline here, as expected.
+    // print_out_string(tmp);
+    
+    printf("Modified country %s\n", countries[index]); // Need to add newline here, as expected.
+    print_out_string(countries[index]);
+    */
 
     return 0;
 }
